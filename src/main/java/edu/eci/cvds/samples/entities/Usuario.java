f94b889f98b4bdbe855d78c0b965180fe7e27987
ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2019-10-29 14:39:38 -0500

    Estructura basica

---
+++ src/main/java/edu/eci/cvds/samples/entities/Usuario.java
@@ -0,0 +1,16 @@
+package edu.eci.cvds.samples.entities;
+
+public class Usuario {
+	private String carnet;
+	private String documento;
+	private String correo;
+	private String nombres;
+	private String apellidos;
+	private String contraseña;
+	private Date ultimo_ingreso;
+	private boolean bloqueado;
+	
+	public Usuario{
+		
+	}
+}

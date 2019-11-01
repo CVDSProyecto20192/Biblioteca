e0aee675345aa3d8ad0f6f3c83e0dd78273dfc4c
migue1994 <migue24co@gmail.com>
2019-10-31 19:59:04 -0500

    Iniciar Sesion

---
+++ src/main/java/edu/eci/cvds/beans/LoginBean.java
@@ -0,0 +1,70 @@
+package edu.eci.cvds.beans;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.apache.shiro.subject.Subject;
+
+
+@ManagedBean(name = "LoginBean")
+@ViewScoped
+
+public class LoginBean implements Serializable{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    
+    private String userName;
+    private String password;
+    private boolean rememberMe;
+
+
+    public void login(){
+        Subject currentUser = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
+
+        token.setRememberMe(true);
+        currentUser.login(token);
+
+        try{
+            currentUser.login(token);
+        } catch(UnknownAccountException e){
+            FacesContext.getCurrentInstance().addMessage("login", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario no encontrado", "Este usuario no se encuentra en nuestra base de datos"));
+        } catch (IncorrectCredentialsException e) {
+            FacesContext.getCurrentInstance().addMessage("login", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Contraseña incorrecta", "La contraseña ingresada no es correcta"));
+        }
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public boolean isRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(boolean rememberMe) {
+        this.rememberMe = rememberMe;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
